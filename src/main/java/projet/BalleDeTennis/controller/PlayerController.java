69dca3e131b763d61d119aa81daba1eefd5856a4
Antoine1206 <antoine.melanger@laposte.net>
2025-01-05 17:18:54 +0100

    Création du template, nettoyage de classe

--- src/main/java/projet/BalleDeTennis/controller/PlayerController.java
+++ src/main/java/projet/BalleDeTennis/controller/PlayerController.java
@@ -1,10 +1,7 @@
 package projet.BalleDeTennis.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import projet.BalleDeTennis.model.Player;
 import projet.BalleDeTennis.services.PlayerService;
 
 @RestController
@@ -15,15 +12,6 @@ public class PlayerController {
 
     public PlayerController(PlayerService playerService) {
         this.playerService = playerService;
-    }
-
-    @GetMapping("/{id}")
-    public Player getPlayerById(@PathVariable Long id) {
-
-        Player player = new Player(1L, "Novak", "Djokovic", 36);
-        playerService.savePlayer(player);
-        playerService.getPlayerById(1L);
-        return player;
     }
 
 
