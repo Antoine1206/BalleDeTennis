aeb8e91c1c106879b7de03786536d9b8484ae05f
Antoine1206 <antoine.melanger@laposte.net>
2025-01-05 17:26:55 +0100

    Commit 1 modifié ajout des attributs

---
+++ src/main/java/projet/BalleDeTennis/model/exerciceVersionning.java
@@ -0,0 +1,36 @@
+package projet.BalleDeTennis.model;
+
+import java.util.Date;
+
+public class exerciceVersionning {
+
+
+    private Long id;
+    private Long age;
+    private Date dateNaissance;
+
+
+    public Date getDateNaissance() {
+        return dateNaissance;
+    }
+
+    public void setDateNaissance(Date dateNaissance) {
+        this.dateNaissance = dateNaissance;
+    }
+
+    public Long getAge() {
+        return age;
+    }
+
+    public void setAge(Long age) {
+        this.age = age;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
